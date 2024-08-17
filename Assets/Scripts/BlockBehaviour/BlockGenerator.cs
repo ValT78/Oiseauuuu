8bd348e02c5e356a7e7cf113f5104f149639bd3c
ValT78 <118465560+ValT78@users.noreply.github.com>
2024-08-17 23:58:48 +0200

    prefab shopManager

--- Assets/Scripts/BlockBehaviour/BlockGenerator.cs
+++ Assets/Scripts/BlockBehaviour/BlockGenerator.cs
@@ -63,11 +63,6 @@ public class BlockGenerator : MonoBehaviour
     {
         return (int)math.max(0, math.round(numberOfCubesInBlock * GameManager.Instance.coastPerCube * GetRandomFloat(GameManager.Instance.variance, 1 / GameManager.Instance.variance) - GameManager.Instance.coastOffset)); ;
     }
-<<<<<<< HEAD
-
-=======
-     
->>>>>>> 332dce634bc2f80d3feaf7efb9705f52e3a59cf8
     public virtual void GenerateBlock(int numberOfCubes)
     {
         GameObject blockParent = this.gameObject; // Utiliser l'objet actuel comme parent
