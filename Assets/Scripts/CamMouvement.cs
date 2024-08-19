377565135daea8d91e2fe0e17eae5ad0e00266a5
Crafteurmax <msansane@gmail.com>
2024-08-19 16:57:57 +0200

    add background

--- Assets/Scripts/CamMouvement.cs
+++ Assets/Scripts/CamMouvement.cs
@@ -11,6 +11,7 @@ public class CamMouvement : MonoBehaviour
     [SerializeField] private Transform transform_bg3;
     [SerializeField] private Transform transform_bg2;
     [SerializeField] private Transform transform_bg1;
+    [SerializeField] private Transform transform_bgm1;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +21,19 @@ public class CamMouvement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(movement.x * speed * Time.deltaTime, movement.y * speed * Time.deltaTime, 0));
-        // clamp y at least to 0
-        if (transform.position.y < 0) transform.Translate(new Vector3(0, -movement.y * speed * Time.deltaTime, 0)); ;
+        Vector3 toMove = new Vector3(movement.x, movement.y, 0) * speed * Time.deltaTime;
+        transform.Translate(toMove);
+        // clamp to border
+        if (transform.position.y < 0 || transform.position.y > 68) transform.Translate(-1 * toMove);
 
         // Background x paralax
-        transform_bg4.Translate(new Vector3(movement.x * speed * Time.deltaTime * 1.1f, 0, 0));
-        transform_bg3.Translate(new Vector3(movement.x * speed * Time.deltaTime * 1.075f, 0, 0));
-        transform_bg2.Translate(new Vector3(movement.x * speed * Time.deltaTime * 1.05f, 0, 0));
-        transform_bg1.Translate(new Vector3(movement.x * speed * Time.deltaTime * 1.025f, 0, 0));
+        transform_bg4.Translate(new Vector3(toMove.x * 1.1f, 0, 0));
+        transform_bg3.Translate(new Vector3(toMove.x * 1.075f, 0, 0));
+        transform_bg2.Translate(new Vector3(toMove.x * 1.05f, 0, 0));
+        transform_bg1.Translate(new Vector3(toMove.x * 1.025f, 0, 0));
+        transform_bgm1.Translate(new Vector3(toMove.x, 0, 0));
 
-        if (transform.position.y < 0) transform.Translate(new Vector3(0, - movement.y * speed * Time.deltaTime, 0)); ;
+
     }
 
     public void Move(InputAction.CallbackContext context)
